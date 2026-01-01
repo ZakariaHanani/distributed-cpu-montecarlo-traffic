9eb38d6d39ac66bf81a7c55c2f5a5e24db243d34
Ouadra <mohamed.oudra3@gmail.com>
2026-01-01 02:43:32 +0100

    chore(scripts): add helper scripts to run services and workers

--- scripts/client.bat
+++ scripts/client.bat
@@ -1,49 +1,16 @@
 @echo off
 setlocal enabledelayedexpansion
 
-rem --------------------------------------------------------
-rem Resolve project root (one level above /scripts)
-rem --------------------------------------------------------
 set "SCRIPT_DIR=%~dp0"
+call "%SCRIPT_DIR%\set-env.bat"
 set "ROOT_DIR=%SCRIPT_DIR%\.."
 
-rem --------------------------------------------------------
-rem Resolve Java: prefer JAVA_HOME, then PATH
-rem --------------------------------------------------------
-set "JAVA_CMD="
-
-rem 1) JAVA_HOME\bin\java.exe
-if defined JAVA_HOME if exist "%JAVA_HOME%\bin\java.exe" (
-    set "JAVA_CMD=%JAVA_HOME%\bin\java.exe"
-)
-
-rem 2) Fallback: java from PATH
-if not defined JAVA_CMD (
-    for /f "delims=" %%i in ('where java 2^>nul') do (
-        set "JAVA_CMD=%%i"
-        goto :have_java
-    )
-)
-
-:have_java
-if not defined JAVA_CMD (
-    echo [ERROR] Java (JDK 17) not found.
-    echo         Please either:
-    echo           - install Java and add ^"java^" to your PATH
-    echo           - or set JAVA_HOME to your JDK folder.
-    exit /b 1
-)
+set "JAVA_CMD=%JAVA_HOME%\bin\java.exe"
 
 echo [client.bat] Using Java: %JAVA_CMD%
 
-rem --------------------------------------------------------
-rem Classpath and main class
-rem --------------------------------------------------------
 set "CP=%ROOT_DIR%\common\target\classes;%ROOT_DIR%\client\target\classes"
-
-echo [client.bat] Using classpath:
-echo   %CP%
-echo.
+echo [client.bat] Using classpath: %CP%
 
 "%JAVA_CMD%" -cp "%CP%" com.grid.client.ClientApp
 
