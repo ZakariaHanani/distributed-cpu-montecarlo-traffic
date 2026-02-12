d042c23b03a5af44c773971340436f8823adca40
Ouadra <mohamed.oudra3@gmail.com>
2026-02-12 14:45:30 +0100

    feat(auth-service): improve simulation results and user management

--- auth-service/src/main/java/com/trafficjam1/authservice/simulation/SimulationResultRepository.java
+++ auth-service/src/main/java/com/trafficjam1/authservice/simulation/SimulationResultRepository.java
@@ -6,5 +6,10 @@ import java.util.Optional;
 
 public interface SimulationResultRepository extends JpaRepository<SimulationResultEntity, Long> {
     Optional<SimulationResultEntity> findBySimulation_Id(Long simulationId);
+
+    @org.springframework.data.jpa.repository.Modifying
+    @org.springframework.transaction.annotation.Transactional
+    @org.springframework.data.jpa.repository.Query("DELETE FROM SimulationResultEntity sr WHERE sr.simulation.user.id = :userId")
+    void deleteByUserId(@org.springframework.data.repository.query.Param("userId") Long userId);
 }
 
